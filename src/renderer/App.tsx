2a434f52c771f1416cc88d0ea0e3d9063b53d527
Sadanand Gopal <sadanandgpl@gmail.com>
2022-10-23 12:10:54 +0530

    initial commit

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,39 +1,32 @@
+import { useState } from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
 
 const Hello = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = (plus: string) => {
+    if (plus === 'plus') {
+      setCount(count + 1);
+    } else {
+      setCount(count - 1);
+    }
+  };
   return (
     <div>
       <div className="Hello">
         <img width="200" alt="icon" src={icon} />
       </div>
       <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+      <div style={{ textAlign: 'center' }}>
+        <button type="button" onClick={() => handleClick('plus')}>
+          Add
+        </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;{count}&nbsp;&nbsp;&nbsp;&nbsp;
+        <button type="button" onClick={() => handleClick('minus')}>
+          -
+        </button>
       </div>
     </div>
   );
